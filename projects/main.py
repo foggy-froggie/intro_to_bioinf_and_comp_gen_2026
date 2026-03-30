1513e713f2da9371dc6ffa21f7116828f82347e7
pkb-pmj <pkb.pmj@gmail.com>
2026-03-30 23:48:05 +0200

    ✨ task 5 (partial)

--- projects/main.py
+++ projects/main.py
@@ -2,6 +2,8 @@
 import pandas as pd
 
 from Bio.Blast import NCBIWWW, NCBIXML
+from Bio import pairwise2
+from Bio.Align import PairwiseAligner
 
 # %%
 prot = "MTEYKLVVVGAGGVGKSALTIQLIQNHFVDEYDPTIEDSYRKQV"
@@ -18,10 +20,27 @@ df = pd.DataFrame(
         hsp.score,
         hsp.align_length / len(prot) * 100,
         alignment.hit_def,
+        hsp.sbjct,
     ] for alignment in blast_record.alignments for hsp in alignment.hsps),
-    columns=["identity", "e-value", "bit score", "coverage", "description"]
+    columns=["identity", "e-value", "bit score", "coverage", "description", "sbjct"]
 )
 
 # %%
 # Task 4 - top 10 BLASTp hits
 df.iloc[:10, :].loc[:, ["identity", "e-value", "description"]]
+
+# %%
+matrix  = substitution_matrices.load("BLOSUM62")
+
+aligner = PairwiseAligner()
+aligner.mode = "global"
+
+aligner.substitution_matrix = matrix
+aligner.open_gap_score = -10
+aligner.extend_gap_score = -0.5
+
+# %%
+# Task 5 - top 5 alignments
+for sbjct in df.loc[:, "sbjct"].iloc[:5]:
+    alignments = aligner.align(prot, sbjct)
+    print(alignments[0])
